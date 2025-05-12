8d8cd4c16f36668a6c2c1e828329838916226d85
Xhoel <xhoel.seitllari@fshnstudent.info>
2025-05-12 16:14:12 +0200

    sticky cart

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -9,7 +9,7 @@ function CartOverview() {
   const totalCartPrice = useSelector(getTotalCartPrice);
   if (!totalCartQuantity) return null;
   return (
-    <div className="flex items-center justify-between bg-stone-800 text-stone-200 uppercase px-4 py-4 sm:px-6 text-sm md:text-base">
+    <div className="flex items-center justify-between bg-stone-800 text-stone-200 uppercase px-4 py-4 sm:px-6 text-sm md:text-base sticky">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6 ">
         <span>{totalCartQuantity}</span>
         <span>{formatCurrency(totalCartPrice)}</span>
